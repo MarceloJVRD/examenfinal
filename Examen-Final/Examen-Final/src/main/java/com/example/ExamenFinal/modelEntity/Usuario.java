73ccfe690f044c17c63d76776703645f7e3c5941
MarceloJVRD <101356620+MarceloJVRD@users.noreply.github.com>
2022-04-21 23:40:55 -0300

    Add files via upload

---
+++ Examen-Final/Examen-Final/src/main/java/com/example/ExamenFinal/modelEntity/Usuario.java
@@ -0,0 +1,36 @@
+package com.example.ExamenFinal.modelEntity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column
+    private String nombres;
+    @Column
+    private boolean admin;
+    @Column
+    private String apellidos;
+    @Column
+    private String password;
+    @Column
+    private String nickname;
+    @Column
+    private Integer edad;
+    @Column
+    private boolean status;
+}
+
+
+
